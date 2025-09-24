2a38ee08916ede6d480a8b9d3bb275e3b27e0c97
Adam Lim <alim4@ufl.edu>
2025-09-24 19:19:12 -0400

    updated testing interface to include front end

--- Idioma-app/Idioma/IdiomaApp.swift
+++ Idioma-app/Idioma/IdiomaApp.swift
@@ -6,12 +6,48 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import FirebaseAuth
+import FirebaseFunctions
+import FirebaseFirestore
+import GoogleSignIn
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    
+    // --- Connect to local emulators for testing ---
+    #if DEBUG
+    print("Connecting to local Firebase emulators.")
+    Auth.auth().useEmulator(withHost: "127.0.0.1", port: 9099)
+    
+    let settings = Firestore.firestore().settings
+    settings.host = "127.0.0.1:8080"
+    settings.isSSLEnabled = false
+    settings.isPersistenceEnabled = false // Useful for testing
+    Firestore.firestore().settings = settings
+    
+    Functions.functions().useEmulator(withHost: "127.0.0.1", port: 5001)
+    #endif
+    
+    return true
+  }
+}
 
 @main
 struct IdiomaApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onOpenURL { url in
+                    // Handle the URL that caused the app to open
+                    // This is crucial for the Google Sign-In redirect
+                    GIDSignIn.sharedInstance.handle(url)
+                }
         }
     }
 }
